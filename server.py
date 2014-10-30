0a340bf2a14ce8fdb000281554026719cc9c9fb4
Flávio Juvenal <flaviojuvenal@gmail.com>
2014-10-30 13:53:03 -0300

    _fetch_track_from_spotify

--- server.py
+++ server.py
@@ -1,22 +1,33 @@
-from flask import Flask, request, jsonify, abort
+from flask import Flask, jsonify, abort
+import requests
 
 app = Flask(__name__)
 
 tracks = {}
 
 @app.route("/api/tracks/", methods=['GET'])
-def get_tracks():
+def tracks_get():
     track_list = sorted(tracks.values(), key=lambda x: x['priority'], reverse=True)
     return jsonify({'tracks': track_list})
 
-@app.route("/api/tracks/<track_id>/", methods=['PUT'])
-def vote(track_id):
-    if track_id not in tracks:
-        tracks[track_id] = {
-            'name': 'test name',
-            'album': 'test album',
+def _fetch_track_from_spotify(track_id):
+    response = requests.get('https://api.spotify.com/v1/tracks/{0}'.format(track_id))
+    if response.status_code == 200:
+        response_json = response.json()
+        return {
+            'name': response_json['name'],
+            'artist': ', '.join(a['name'] for a in response_json['artists']),
             'priority': 0
         }
+
+@app.route("/api/tracks/<track_id>/", methods=['PUT'])
+def track_put(track_id):
+    if track_id not in tracks:
+        track = _fetch_track_from_spotify(track_id)
+        if not track:
+            abort(400)
+
+        tracks[track_id] = track
 
     tracks[track_id]['priority'] += 1
 
