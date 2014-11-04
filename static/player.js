034ff00065d166eced8ba5a683cb5bccd3cc5808
Flávio Juvenal <flaviojuvenal@gmail.com>
2014-11-04 14:41:08 -0300

    Refactoring player.js

--- static/player.js
+++ static/player.js
@@ -1,72 +1,81 @@
-function get_next() {
-    $.ajax({
+(function () {
+  var HubrocksAPI = (function () {
+    var getNext = function () {
+      return $.ajax({
         url: API_URL + '/tracks/next/',
         type: 'GET'
-    }).done(function (json) {
-        if (json.next) {
-            set_now_playing(json.next.deezer_id).then(function () {
-                delete_next(json.next.deezer_id);
-            });
+      });
+    };
 
-            DZ.player.playTracks([json.next.deezer_id]);
-        } else {
-            console.log('no next, will try again...');
-            fail();
-        }
+    var deleteNext = function (deezer_id) {
+      return $.ajax({
+        url: API_URL + '/tracks/' + deezer_id + '/',
+        type: 'DELETE'
+      });
+    };
+
+    var setNowPlaying = function (deezer_id) {
+      return $.ajax({
+        url: API_URL + '/tracks/' + deezer_id + '/now-playing/',
+        type: 'PUT'
+      });
+    };
+
+    var deleteNowPlaying = function (deezer_id) {
+      $.ajax({
+        url: API_URL + '/tracks/' + deezer_id + '/now-playing/',
+        type: 'DELETE'
+      });
+    };
+
+    return {
+      getNext: getNext,
+      deleteNext: deleteNext,
+      setNowPlaying: setNowPlaying,
+      deleteNowPlaying: deleteNowPlaying
+    };
+  }());
+
+  var popNextAndPlay = function () {
+    HubrocksAPI.getNext().done(function (json) {
+      if (json.next) {
+        HubrocksAPI.setNowPlaying(json.next.deezer_id)
+          .then(function () {
+            HubrocksAPI.deleteNext(json.next.deezer_id);
+          });
+
+        DZ.player.playTracks([json.next.deezer_id]);
+      } else {
+        console.log('no next, will try again...');
+        fail();
+      }
     }).fail(fail);
 
     function fail() {
-        setTimeout(function () {
-            get_next();
-        }, 3000);
+      setTimeout(function () {
+        popNextAndPlay();
+      }, 3000);
     }
-}
+  };
 
-function delete_next(deezer_id) {
-    $.ajax({
-        url: API_URL + '/tracks/' + deezer_id + '/',
-        type: 'DELETE'
-    }).done(function () {
-        console.log('deleted next');
-    });
-}
-
-function set_now_playing(deezer_id) {
-    return $.ajax({
-        url: API_URL + '/tracks/' + deezer_id + '/now-playing/',
-        type: 'PUT'
-    }).done(function () {
-        console.log('set now-playing');
-    });
-}
-
-function delete_now_playing(deezer_id) {
-    $.ajax({
-        url: API_URL + '/tracks/' + deezer_id + '/now-playing/',
-        type: 'DELETE'
-    }).done(function () {
-        console.log('deleted now-playing');
-    });
-}
-
-function onPlayerLoaded() {
-    DZ.Event.subscribe('track_end', function (currentIndex) {
-        var track = DZ.player.getCurrentTrack();
-        delete_now_playing(track.id);
-        get_next();
-    });
-    get_next();
-}
-
-DZ.init({
-    appId  : '8',
-    channelUrl : 'http://developers.deezer.com/examples/channel.php',
-    player : {
-        container : 'player',
-        cover : true,
-        playlist : true,
-        width : 650,
-        height : 300,
-        onload : onPlayerLoaded
-    }
-});
+  DZ.init({
+      appId  : '8',
+      channelUrl : 'http://developers.deezer.com/examples/channel.php',
+      player : {
+          container : 'player',
+          cover : true,
+          playlist : true,
+          width : 650,
+          height : 300,
+          onload : function () {
+            DZ.Event.subscribe('track_end', function (currentIndex) {
+              var track = DZ.player.getCurrentTrack();
+              HubrocksAPI.deleteNowPlaying(track.id);
+              popNextAndPlay();
+            });
+            
+            popNextAndPlay();
+          }
+      }
+  });  
+}());
