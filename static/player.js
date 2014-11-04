f6d555419cc29aaccb24f1de31ca169379d8933a
Flávio Juvenal <flaviojuvenal@gmail.com>
2014-11-04 14:19:29 -0300

    Refactor deezer.html

--- static/player.js
+++ static/player.js
@@ -49,38 +49,15 @@ function delete_now_playing(deezer_id) {
     });
 }
 
-
-$(document).ready(function(){
-    $("#controlers input").attr('disabled', true);
-    $("#slider_seek").click(function(evt,arg){
-        var left = evt.offsetX;
-        DZ.player.seek((evt.offsetX/$(this).width()) * 100);
-    });
-});
-function event_listener_append() {
-    var pre = document.getElementById('event_listener');
-    var line = [];
-    for (var i = 0; i < arguments.length; i++) {
-        line.push(arguments[i]);
-    }
-    pre.innerHTML += line.join(' ') + "\n";
-}
 function onPlayerLoaded() {
-    $("#controlers input").attr('disabled', false);
-    event_listener_append('player_loaded');
-    
     DZ.Event.subscribe('track_end', function (currentIndex) {
         var track = DZ.player.getCurrentTrack();
         delete_now_playing(track.id);
         get_next();
     });
     get_next();
-    
-    DZ.Event.subscribe('player_position', function(arg){
-        event_listener_append('position', arg[0], arg[1]);
-        $("#slider_seek").find('.bar').css('width', (100*arg[0]/arg[1]) + '%');
-    });
 }
+
 DZ.init({
     appId  : '8',
     channelUrl : 'http://developers.deezer.com/examples/channel.php',
