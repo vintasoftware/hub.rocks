1128433b643bbdd49e607a3d89ce31b5729ef13b
Flávio Juvenal <flaviojuvenal@gmail.com>
2015-02-20 17:53:02 -0300

    PlayerView with SuperuserRequiredMixin

--- tracks/views.py
+++ tracks/views.py
@@ -1,9 +1,13 @@
 from django.views import generic
 
+from braces.views import LoginRequiredMixin, SuperuserRequiredMixin
+
 
 class VoteView(generic.TemplateView):
     template_name = 'tracks/vote.html'
 
 
-class PlayerView(generic.TemplateView):
+class PlayerView(LoginRequiredMixin,
+                 SuperuserRequiredMixin,
+                 generic.TemplateView):
     template_name = 'tracks/player.html'
